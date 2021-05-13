51b12f71e7dad87dc347cfca242d9b01a3ce91cc
AMAN SINGHAL <65446479+AmanSinghal927@users.noreply.github.com>
2021-05-13 11:52:37 +0530

    Update cmd.sh
    Updated for using with Google Colab

--- cmd.sh
+++ cmd.sh
@@ -1,127 +1,109 @@
-#!/bin/bash
-#SBATCH --job-name=bt
-#SBATCH --partition long
-#SBATCH --account shashanks
-#SBATCH --gres=gpu:4
-#SBATCH --nodes=1
-#SBATCH --mem=40G
-#SBATCH --time 3-00:00:00
-#SBATCH --signal=B:HUP@600
-##SBATCH -w gnode37
+#!bin/bash
 
-
-module add use.own
-module load python/3.7.0
-module load pytorch/1.1.0
-
-<<CMT
-IMPORTS=(
-    filtered-iitb.tar
-    ilci.tar
-    national-newscrawl.tar
-    ufal-en-tam.tar
-    wat-ilmpc.tar
-    bible-en-te.tar
-    eenadu-en-te.tar
-)
-CMT
+#module load python/3.7.0
+#module load pytorch/1.1.0
 
 IMPORTS=(
-    odiencorp.tar
+    # filtered-iitb.tar
+    #ilci.tar
+    # national-newscrawl.tar
+    #ufal-en-tam.tar
+    #wat-ilmpc.tar
 )
 
-LOCAL_ROOT="/ssd_scratch/cvit/$USER"
+LOCAL_ROOT='/content/drive/My Drive/IIITH/ilmulti'
 REMOTE_ROOT="ada:/share1/dataset/text"
 
-mkdir -p $LOCAL_ROOT/{data,checkpoints}
+
+#mkdir -p $LOCAL_ROOT/{data,checkpoints}
 
 DATA=$LOCAL_ROOT/data
-CHECKPOINTS=$LOCAL_ROOT/checkpoints
+#CHECKPOINTS=$LOCAL_ROOT/ufal-transformer-big/checkpoints
+CHECKPOINTS='/content/drive/My\ Drive/IIITH/ilmulti/checkpoints_hi_full_bt'
+# RESULT = '/content/drivr/My\ Drive/'
 
-rsync -r /home/shashanks/ilci/ $DATA/ilci/
+#function copy {
+#    for IMPORT in ${IMPORTS[@]}; do
+#        rsync --progress $REMOTE_ROOT/$IMPORT $DATA/
+#        tar_args="$DATA/$IMPORT -C $DATA/"
+#        tar -df $tar_args 2> /dev/null || tar -kxvf $tar_args
+#    done
+#}
 
-#rsync -rvz ada:/share1/shashanks/checkpoints/mmall/checkpoint_last.pt $CHECKPOINTS/
-
-function copy {
-    for IMPORT in ${IMPORTS[@]}; do
-        rsync --progress $REMOTE_ROOT/$IMPORT $DATA/
-        tar_args="$DATA/$IMPORT -C $DATA/"
-        tar -df $tar_args 2> /dev/null || tar -kxvf $tar_args
-    done
-}
-
-function _export {
-    ssh $USER@ada "mkdir -p ada:/share1/$USER/checkpoints/pib"
-    rsync -rvz $CHECKPOINTS/checkpoint_{best,last}.pt ada:/share1/$USER/checkpoints/pib/
-}
-
-trap "_export" SIGHUP
-copy
+# copy
 export ILMULTI_CORPUS_ROOT=$DATA
-
-python3 preprocess_cvit.py config.yaml
-
-
-ARCH='transformer'
-MAX_TOKENS=3500
-LR=1e-3
-UPDATE_FREQ=128
-MAX_EPOCHS=200
 
 set -x
 function train {
-    python3 train.py \
+    python3 /content/drive/My\ Drive/IIITH/fairseq-ilmt/train.py \
         --task shared-multilingual-translation \
-        --share-all-embeddings \
         --num-workers 0 \
-        --arch $ARCH \
-        --max-tokens $MAX_TOKENS --lr $LR --min-lr 1e-9 \
-        --optimizer adam --adam-betas '(0.9, 0.98)' \
-        --save-dir $CHECKPOINTS \
+        --arch transformer \
+        --max-tokens 5000 --lr 1e-4 --min-lr 1e-9 \
+        --optimizer adam \
+        --save-dir /content/drive/My\ Drive/IIITH/ilmulti/checkpoints_pa \
         --log-format simple --log-interval 200 \
-        --dropout 0.1 --attention-dropout 0.1 --activation-dropout 0.1 \
-        --lr-scheduler inverse_sqrt \
-        --clip-norm 0.1 \
-        --ddp-backend no_c10d \
-        --update-freq $UPDATE_FREQ \
-        --max-epoch $MAX_EPOCHS \
         --criterion label_smoothed_cross_entropy \
-        config.yaml
-
+        --dropout 0.1 --attention-dropout 0.1 --activation-dropout 0.1 \
+        --ddp-backend no_c10d \
+        --update-freq 2 \
+        --share-all-embeddings \
+        --restore-file /content/drive/My\ Drive/IIITH/ilmulti/checkpoints_pa/checkpoint_66.pt \
+        data=/content/drive/My\ Drive/IIITH/fairseq-ilmt/config.yaml
+        #--reset-optimizer 
 }
 
-    #    --reset-optimizer \
-    #    --reset-lr-scheduler \
 
 function _test {
-    python3 generate.py config.yaml \
+    a=$1
+    b=$2
+    for i in {34..35}
+    # for i in 66
+    do
+    echo "Evaluating checkpoint $i"
+    python3 /content/drive/My\ Drive/IIITH/fairseq-ilmt/generate.py /content/drive/My\ Drive/IIITH/fairseq-ilmt/config.yaml \
         --task shared-multilingual-translation  \
-        --path $CHECKPOINTS/checkpoint_last.pt > ufal-gen.out
-    cat ufal-gen.out \
+        -s en -t hi \
+        --path "/content/drive/My Drive/IIITH/ilmulti/checkpoints_pa/checkpoint$i.pt" > gen.out
+    cat gen.out \
         | grep "^H" | sed 's/^H-//g' | sort -n | cut -f 3 \
         | sed 's/ //g' | sed 's/▁/ /g' | sed 's/^ //g' \
-            > ufal-test.hyp
-    cat ufal-gen.out \
-        | grep "^T" | sed 's/^T-//g' | sort -n | cut -f 2 \
-        | sed 's/ //g' | sed 's/▁/ /g' | sed 's/^ //g' \
-            > ufal-test.ref
+            > test.hyp
+    #cat gen.out \
+    #    | grep "^T" | sed 's/^T-//g' | sort -n | cut -f 2 \
+    #    | sed 's/ //g' | sed 's/▁/ /g' | sed 's/^ //g' \
+    #        > test.ref
 
-    split -d -l 2000 ufal-test.hyp hyp.ufal.
-    split -d -l 2000 ufal-test.ref ref.ufal.
+    #split -d -l 1260 test.hyp hyp.
+    #split -d -l 1260 test.ref ref.
 
-    # perl multi-bleu.perl ref.ufal.00 < hyp.ufal.00 
-    # perl multi-bleu.perl ref.ufal.01 < hyp.ufal.01 
+    # perl indic_nlp_library/indicnlp/contrib/wat/multi-bleu.perl ref.00 < hyp.00 
+    # perl indic_nlp_library/indicnlp/contrib/wat/multi-bleu.perl ref.01 < hyp.01 
 
-    python3 -m indicnlp.contrib.wat.evaluate \
-        --reference ref.ufal.00 --hypothesis hyp.ufal.00 
-    python3 -m indicnlp.contrib.wat.evaluate \
-        --reference ref.ufal.01 --hypothesis hyp.ufal.01 
+    #python3 -m indicnlp.contrib.wat.evaluate \
+    #    --reference ben-test.ref --hypothesis ben-test.hyp 
 
+    #python3 -m indicnlp.contrib.wat.evaluate \
+    #    --reference ref.00 --hypothesis hyp.00 
+    #python3 -m indicnlp.contrib.wat.evaluate \
+    #    --reference ref.01 --hypothesis hyp.01 
+
+    # python3 -m wateval.wateval.evaluate \
+    # python3 -m content.drive.My\ Drive.IIITH.fairseq-ilmt.wateval/wateval/evaluate.py \
+    cd /content/drive/My\ Drive/IIITH/fairseq-ilmt/wateval
+    python3 -m wateval.evaluate \
+         --reference test.ref \
+         --hypothesis test.hyp
+    done
 }
 
-ARG=$1
-eval "$1"
+function _backtranslate {
+    python3 generate.py config.yaml \
+        --task shared-multilingual-translation  \
+        --path $CHECKPOINTS/checkpoint13.pt > $RESULTS/backtranslated.txt
+}
+#ARG=$1
+#eval "$1"
 # _test
 
-wait
-_export
+train
